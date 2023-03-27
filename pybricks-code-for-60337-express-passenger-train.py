d17cda7aab8a3acfa7dba8bfd156ef4a65d4a77b
Jessendelft <62891470+Jessendelft@users.noreply.github.com>
2023-03-27 13:59:17 +0200

    Update pybricks-code-for-60337-express-passenger-train.py
    Made your code more dynamic. This allows for multiple stations and multiple yellow/red blocks for more advanced layouts.

--- pybricks-code-for-60337-express-passenger-train.py
+++ pybricks-code-for-60337-express-passenger-train.py
@@ -9,40 +9,36 @@ station_stop_time_ms = 2500
 eol_stop_time_ms = 5000
 forward_speed = 20
 check_color_interval_ms = 20
+forward_direction = True
 
-def check_for_color(color):
-    while sensor.color() != color:
-        wait(check_color_interval_ms)
-
-while True:
-    print("looking for green")
-    check_for_color(Color.GREEN)
-
-    print("green detected, at station, stopping and continuing")
-    motor.brake()
-    wait(station_stop_time_ms)
-    motor.dc(forward_speed)
-
-    print("looking for yellow")
-    check_for_color(Color.YELLOW)
-
-    print("yellow detected, at end of line, stopping and going back to station")
-    motor.brake()
-    wait(eol_stop_time_ms)
-    motor.dc(-forward_speed)
-
-    print("looking for green")
-    check_for_color(Color.GREEN)
-
-    print("green detected, at station, stopping and continuing")
-    motor.brake()
-    wait(station_stop_time_ms)
-    motor.dc(-forward_speed)
-
-    print("looking for red")
-    check_for_color(Color.RED)
-
+def do_on_red():
     print("red detected, at end of line, stopping and going back to station")
     motor.brake()
     wait(eol_stop_time_ms)
-    motor.dc(forward_speed)
+    forward_direction = False
+    
+def do_on_yellow():
+    print("yellow detected, at end of line, stopping and going back to station")
+    motor.brake()
+    wait(eol_stop_time_ms)
+    forward_direction = True
+
+def do_on_green():
+    print("green detected, at station, stopping and continuing")
+    motor.brake()
+    wait(station_stop_time_ms)
+
+while True:
+    if sensor.color() = Color.GREEN:
+        do_on_green()
+    elif sensor.color() = Color.YELLOW:
+        do_on_yellow()
+    elif sensor.color() = Color.RED:
+        do_on_red()
+    else:
+        if forward_direction:
+            motor.dc(forward_speed)
+        else:
+            motor.dc(-forward_speed)
+    wait(check_color_interval_ms)
+
